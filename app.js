0bd2065337221eee27774c8d9a8c2b8c4f3689f1
kashifjhanjaria <kashifmayo@outlook.com>
2022-08-26 17:12:41 +0500

    first Commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const btn = document.getElementById("btn");
+const light = document.getElementById("light");
+
+function toggleBtn() {
+  btn.classList.toggle("active");
+  light.classList.toggle("on");
+}
